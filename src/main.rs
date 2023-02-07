bcee706a215554f0e2493a8b2b4c828930705cc6
Fanghao Yang <yangfanghao@gmail.com>
2023-02-07 00:49:05 -0500

    add test cases for bad requests.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use ark_linear_sumcheck::ml_sumcheck::protocol::{ListOfProductsOfPolynomials, Po
 use ark_linear_sumcheck::ml_sumcheck::{MLSumcheck, Proof};
 use ark_linear_sumcheck::Error as SumCheckError;
 use ark_poly::DenseMultilinearExtension;
-use ark_std::rand;
+use ark_std::rand::rngs::StdRng;
 use ark_std::rand::{Rng, SeedableRng};
 use ark_std::{Zero, One};
+use rand;
 use generic::{Data, TableOperation};
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -72,10 +73,10 @@ impl TableOperation<Fr> for Data<Fr> {
     }
 }
 
-fn generate_random_seed(seed: &[u8; 32], exp: usize) -> Vec<Fr> {
+fn gen_random_from_seed(seed: &[u8; 32], exp: usize) -> Vec<Fr> {
     // generate random vector with seed.
     let mut vec_r = Vec::new();
-    let mut rng = rand::rngs::StdRng::from_seed(seed.to_owned());
+    let mut rng = StdRng::from_seed(seed.to_owned());
     let exp = if exp > 64 { 64 } else { exp };
     let base: usize = 2;
     let limit: usize = base.pow(exp as u32);
@@ -98,8 +99,15 @@ impl Victor {
         }
     }
 
-    pub fn verify_proof(&self, proof: &Proof<Fr>, info: &PolynomialInfo, product: Vec<Fr>) ->  Result<SubClaim<Fr>> {
+    pub fn verify_proof(&self, proof: &Proof<Fr>, info: &PolynomialInfo) ->  Result<SubClaim<Fr>> {
         MLSumcheck::<Fr>::verify(info, Fr::zero(), proof).context("Cannot verify the proof!")
+    }
+
+    pub fn gen_random(&self) -> usize {
+        let base: usize = 2;
+        let limit: usize = base.pow(self.exp as u32);
+        let mut rng = rand::thread_rng();
+        rng.gen_range(0..limit)
     }
 }
 
@@ -126,6 +134,7 @@ impl Peggy {
         seed: &[u8; 32],
         a: &str,
         b: &str,
+        prod: Vec<Fr>,
     ) -> Result<(Proof<Fr>, PolynomialInfo), SumCheckError> {
         let data = self.data.as_ref();
         let a_string = a.to_string();
@@ -137,14 +146,13 @@ impl Peggy {
         let mle_b = DenseMultilinearExtension::<Fr>::from_evaluations_vec(data.exp, vec_b);
         let rc_b = Rc::new(mle_b);
         // generate random vector with seed.
-        let vec_r = generate_random_seed(seed, data.exp);
+        let vec_r = gen_random_from_seed(seed, data.exp);
         let mle_r = DenseMultilinearExtension::<Fr>::from_evaluations_vec(data.exp, vec_r);
         let rc_r = Rc::new(mle_r);
         let mut list = ListOfProductsOfPolynomials::<Fr>::new(data.exp);
         let product = vec![rc_r.clone(), rc_a.clone(), rc_b.clone()];
         list.add_product(product, Fr::one());
-        let vec_p = data.product(&a_string, &b_string).unwrap();
-        let mle_p = DenseMultilinearExtension::<Fr>::from_evaluations_vec(data.exp, vec_p);
+        let mle_p = DenseMultilinearExtension::<Fr>::from_evaluations_vec(data.exp, prod);
         let rc_p = Rc::new(mle_p);
         let product = vec![rc_r.clone(), rc_p.clone()];
         list.add_product(product, -Fr::one());
@@ -176,6 +184,7 @@ impl Penelope {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Add;
     use super::*;
     use ark_std::{One, UniformRand, Zero};
 
@@ -201,7 +210,7 @@ mod tests {
     }
 
     #[test]
-    fn test_proof(){
+    fn test_good_proof(){
         let exp = 3;
         let victor = Victor::new(exp);
         // prepare data
@@ -210,8 +219,8 @@ mod tests {
             1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0,
         ];
-        let vec_a = generate_random_seed(&data_seed, exp);
-        let vec_b = generate_random_seed(&data_seed, exp);
+        let vec_a = gen_random_from_seed(&data_seed, exp);
+        let vec_b = gen_random_from_seed(&data_seed, exp);
         data.add_column("A", Some(vec_a));
         data.add_column("B", Some(vec_b));
         let data = Rc::new(data);
@@ -224,10 +233,65 @@ mod tests {
             0, 0, 0, 123,
         ];
         // step 4 & 5.
-        let (proof, info) = peggy.generate_proof(&victor_seed, "A", "B").unwrap();
+        let (proof, info) = peggy.generate_proof(&victor_seed, "A", "B", request_product.clone()).unwrap();
         assert_eq!(MLSumcheck::<Fr>::extract_sum(&proof), Fr::zero(), "Sum is not zero");
         // step 6: victor verify the proof.
-        victor.verify_proof(&proof, &info, request_product).unwrap();
-        // victor
+        victor.verify_proof(&proof, &info).unwrap();
+        // step 7: victor generate a random index q.
+        let q = victor.gen_random();
+        // step 8: evaluations
+        let penelope = Penelope::new(data.clone());
+        let aq = penelope.generate_eval("A", q);
+        let bq = penelope.generate_eval("B", q);
+        // step 9
+        let vec_r = gen_random_from_seed(&victor_seed, data.exp);
+        let rq = vec_r[q].clone();
+        let pq = request_product[q].clone();
+        let fq = rq * (aq * bq - pq);
+        assert_eq!(fq, Fr::zero());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_proof(){
+        let exp = 3;
+        let victor = Victor::new(exp);
+        // prepare data
+        let mut data = Data::new(exp);
+        let data_seed = [
+            1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0,
+        ];
+        let vec_a = gen_random_from_seed(&data_seed, exp);
+        let vec_b = gen_random_from_seed(&data_seed, exp);
+        data.add_column("A", Some(vec_a));
+        data.add_column("B", Some(vec_b));
+        let data = Rc::new(data);
+        let peggy = Peggy::new(data.clone());
+        // step 2: victor request product from peggy.
+        let request_product = peggy.compute_product("A", "B");
+        // step 3: victor generate a seed and send it to peggy.
+        let victor_seed = [
+            1, 2, 4, 8, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 123,
+        ];
+        // step 4 & 5.
+        let bad_prod: Vec<Fr> = request_product.into_iter().map(|x|x.add(&Fr::one())).collect();
+        let (proof, info) = peggy.generate_proof(&victor_seed, "A", "B", bad_prod.clone()).unwrap();
+        assert_ne!(MLSumcheck::<Fr>::extract_sum(&proof), Fr::zero(), "Sum is zero");
+        // step 7: victor generate a random index q.
+        let q = victor.gen_random();
+        // step 8: evaluations
+        let penelope = Penelope::new(data.clone());
+        let aq = penelope.generate_eval("A", q);
+        let bq = penelope.generate_eval("B", q);
+        // step 9
+        let vec_r = gen_random_from_seed(&victor_seed, data.exp);
+        let rq = vec_r[q].clone();
+        let pq = bad_prod[q].clone();
+        let fq = rq * (aq * bq - pq);
+        assert_ne!(fq, Fr::zero(), "Evaluation is good");
+        // step 6: victor verify the proof.
+        victor.verify_proof(&proof, &info).unwrap();
     }
 }
